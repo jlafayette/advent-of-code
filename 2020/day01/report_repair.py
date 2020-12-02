3d638d2818b952d76360fb5248051118532f7cbb
Josh LaFayette <joshlafayette@gmail.com>
2020-12-01 23:14:41 -0800

    Refactor day1 (combine solutions)
    Combine solutions into one file.

--- 2020/day01/report_repair.py
+++ 2020/day01/report_repair.py
@@ -1,4 +1,6 @@
-"""Find the two entries that sum to 2020 and then multiply those two numbers
+"""Day 1: Report Repair
+
+Find the two entries that sum to 2020 and then multiply those two numbers
 together.
 
 For example, suppose your expense report contained the following:
@@ -15,6 +17,15 @@ them together produces 1721 * 299 = 514579, so the correct answer is 514579.
 
 Of course, your expense report is much larger. Find the two entries that sum to
 2020; what do you get if you multiply them together?
+
+
+--- Part Two ---
+Using the above example again, the three entries that sum to 2020 are 979, 366,
+and 675. Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum to
+2020?
+
 """
 from pathlib import Path
 from typing import Optional, List
@@ -22,25 +33,43 @@ from typing import Optional, List
 
 def main() -> None:
     # read from input and convert to list of integers
-    input_file = Path(__file__).parent / "report_repair_input"
+    entries = get_entries()
+
+    # find product of entries that sum to 2020
+    result2 = product_of_2_entries(entries)
+    print(result2)
+    result3 = product_of_3_entries(entries)
+    print(result3)
+
+
+def get_entries() -> List[int]:
+    input_file = Path(__file__).parent / "input"
     text = input_file.read_text()
     split = text.split()
     entries = [int(entry) for entry in split]
-
-    # find product of entries that sum to 2020
-    result = product_of_entries(entries)
-
-    # print result
-    print(result)
+    return entries
 
 
-def product_of_entries(entries: List[int]) -> Optional[int]:
+def product_of_2_entries(entries: List[int]) -> Optional[int]:
     for index1, entry1 in enumerate(entries):
         for index2, entry2 in enumerate(entries):
             if index1 == index2:
                 continue
             if entry1 + entry2 == 2020:
                 return entry1 * entry2
+    else:
+        return None
+
+
+def product_of_3_entries(entries: List[int]) -> Optional[int]:
+    for index1, entry1 in enumerate(entries):
+        for index2, entry2 in enumerate(entries):
+            for index3, entry3 in enumerate(entries):
+                # make sure we aren't comparing the same entry
+                if len({index1, index2, index3}) != 3:
+                    continue
+                if entry1 + entry2 + entry3 == 2020:
+                    return entry1 * entry2 * entry3
     else:
         return None
 
