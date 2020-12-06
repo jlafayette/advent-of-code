06779703c41b4382f14047b76351b9e2fd4e402d
Josh LaFayette <joshlafayette@gmail.com>
2020-12-05 21:23:08 -0800

    Solve day 6

---
+++ 2020/day06/custom_customs.py
@@ -0,0 +1,66 @@
+"""Initial solution."""
+from pathlib import Path
+
+
+def anyone_yes(group):
+    chars = set()
+    for char in group:
+        if char == "\n":
+            continue
+        chars.add(char)
+    return len(chars)
+
+
+def part1(data: str):
+    groups = data.strip().split("\n\n")
+    counts = []
+    for group in groups:
+        counts.append(anyone_yes(group))
+    return sum(counts)
+
+
+def everyone_yes(group):
+    combined = group.replace("\n", "")
+    answers = set()
+    for char in combined:
+        answers.add(char)
+    count = 0
+    for a in answers:
+        if all([a in line for line in group.split()]):
+            count += 1
+    return count
+
+
+def test_part2():
+    data = """abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b"""
+    assert part2(data) == 6
+
+
+def part2(data: str):
+    groups = data.strip().split("\n\n")
+    return sum([everyone_yes(group) for group in groups])
+
+
+def main():
+    input_file = Path(__file__).parent / "input"
+    data = input_file.read_text()
+    print(part1(data))
+    print(part2(data))
+
+
+if __name__ == "__main__":
+    main()
