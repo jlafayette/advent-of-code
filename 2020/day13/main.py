c97842fc30ab1ed4f1d8c4c8c81982f3c2513a05
Josh LaFayette <joshlafayette@gmail.com>
2020-12-18 17:46:05 -0800

    Finish solution for day 13

--- 2020/day13/main.py
+++ 2020/day13/main.py
@@ -4,7 +4,9 @@ https://adventofcode.com/2020/day/13
 
 """
 import sys
+from itertools import count
 from pathlib import Path
+from typing import NamedTuple, List
 
 import pytest
 
@@ -43,6 +45,7 @@ def part1(data: str):
 
 
 @pytest.mark.parametrize("schedule,expected", [
+    ("123\n17,x,13", 102),
     ("123\n17,x,13,19", 3417),
     ("123\n67,7,59,61", 754018),
     ("123\n67,x,7,59,61", 779210),
@@ -53,42 +56,33 @@ def test_part2(schedule, expected):
     assert part2(schedule) == expected
 
 
+class Bus(NamedTuple):
+    id: int
+    index: int
+
+
+def parse_input(data):
+    _, schedule = data.strip().split("\n")
+    buses = [
+        Bus(id=int(s), index=i)
+        for i, s in enumerate(schedule.split(","))
+        if s != "x"
+    ]
+    return buses
+
+
 def part2(data: str):
+    first, *rest = parse_input(data)
 
-    # TODO: Find a more efficient solution. Currently this solves the test
-    #       examples but is way to slow for the full data for the challenge.
+    def recursive(buses: List[Bus], start: int, step: int) -> int:
+        if not buses:
+            return start
+        bus = buses[0]
+        for i in count(start, step):
+            if (i + bus.index) % bus.id == 0:
+                return recursive(buses[1:], i, bus.id*step)
 
-    _, bus_id_strings = data.strip().split("\n")
-    bus_id_strings = bus_id_strings.split(",")
-    bus_ids = []
-    for id_str in bus_id_strings:
-        try:
-            bus_ids.append(int(id_str))
-        except ValueError:
-            bus_ids.append(None)
-
-    largest_id = 0
-    largest_index = -1
-    for i, bus_id in enumerate(bus_ids):
-        if bus_id is None:
-            continue
-        if bus_id > largest_id:
-            largest_id = bus_id
-            largest_index = i
-
-    def counter():
-        mult = 1
-        while True:
-            yield (largest_id * mult) - largest_index
-            mult += 1
-
-    for t in counter():
-        for i, bus_id in enumerate(bus_ids):
-            if bus_id is not None:
-                if (t + i) % bus_id != 0:
-                    break
-        else:
-            return t
+    return recursive(rest, first.index, first.id)
 
 
 def main():
