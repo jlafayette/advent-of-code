09f200adf468a7af850ad20e17716b4088a37d83
Josh LaFayette <joshlafayette@gmail.com>
2021-02-25 00:01:43 -0800

    finish zig solution for day 6

--- 2020/day06/main.zig
+++ 2020/day06/main.zig
@@ -6,12 +6,20 @@ pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = &arena.allocator;
+
     var it = std.mem.split(input, "\r\n\r\n");
     var sum: usize = 0;
     while (it.next()) |group| {
         sum += try anyoneYes(allocator, group);
     }
     print("part 1: {d}\n", .{sum}); // 6763
+
+    it = std.mem.split(input, "\r\n\r\n");
+    sum = 0;
+    while (it.next()) |group| {
+        sum += try everyoneYes(allocator, group);
+    }
+    print("part 2: {d}\n", .{sum}); // 3512
 }
 
 fn anyoneYes(allocator: *std.mem.Allocator, group: []const u8) !usize {
@@ -36,19 +44,39 @@ fn anyoneYes(allocator: *std.mem.Allocator, group: []const u8) !usize {
 const testing = std.testing;
 
 test "anyoneYes" {
-    // print("\n", .{});
     const actual = try anyoneYes(testing.allocator, "abcx\nabcy\nabcz");
-    // print("actual: {d}\n", .{actual});
     testing.expectEqual(@as(usize, 6), actual);
-    // var map = std.AutoHashMap(u8, u32).init(testing.allocator);
-    // defer map.deinit();
-    // try map.put(1, 1);
-    // try map.put(2, 2);
-    // const count = (map.get(2) orelse 0) + 1;
-    // try map.put(2, count);
-    // var iter = map.iterator();
-    // while (iter.next()) |kv| {
-    //     print("key: {d}, value: {d}\n", .{kv.key, kv.value});
-    // }
-    // print("\n\n", .{});
+}
+
+fn everyoneYes(allocator: *std.mem.Allocator, group: []const u8) !usize {
+    var map = std.AutoHashMap(u8, u32).init(allocator);
+    defer map.deinit();
+    var peopleCount: usize = 1;
+    for (group) |byte| {
+        switch (byte) {
+            '\n' => {
+                peopleCount += 1;
+            },
+            '\r' => {
+                continue;
+            },
+            else => {
+                const count = (map.get(byte) orelse 0) + 1;
+                try map.put(byte, count);
+            },
+        }
+    }
+    var count: usize = 0;
+    var it = map.iterator();
+    while (it.next()) |kv| {
+        if (kv.value == peopleCount) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+test "everyoneYes" {
+    const actual = try everyoneYes(testing.allocator, "abcx\nabcy\nabcz");
+    testing.expectEqual(@as(usize, 3), actual);
 }
