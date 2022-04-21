ce3fba621d65ccb226026b8210e2b6e3c9714203
Josh LaFayette <joshlafayette@gmail.com>
2022-04-20 17:53:07 -0700

    Day1 part2 (2021) complete!

--- 2021/day01/main.py
+++ 2021/day01/main.py
@@ -1,3 +1,4 @@
+from cmath import exp
 from pathlib import Path
 
 
@@ -31,6 +32,37 @@ def test_part1():
     assert part1(TEST_INPUT) == 7
 
 
+def part2(data: str) -> int:
+    numbers = [int(x) for x in data.split("\n") if x]
+    increase_count = 0
+    for i, n1 in enumerate(numbers):
+        try:
+            n2 = numbers[i+1]
+        except IndexError:
+            break
+        try:
+            n3 = numbers[i+2]
+        except IndexError:
+            break
+        try:
+            n4 = numbers[i+3]
+        except IndexError:
+            break
+
+        sum1 = n1 + n2 + n3
+        sum2 = n2 + n3 + n4
+
+        if sum2 > sum1:
+            increase_count += 1
+    return increase_count
+
+
+def test_part2():
+    result = part2(TEST_INPUT)
+    expected = 5
+    assert result == expected
+
+
 def main():
     # read puzzle input from file
     input_file = Path(__file__).parent / "input"
@@ -38,6 +70,9 @@ def main():
 
     part1_answer = part1(contents)
     print(part1_answer)
+
+    part2_answer = part2(contents)
+    print(part2_answer)
 
 
 if __name__ == "__main__":
