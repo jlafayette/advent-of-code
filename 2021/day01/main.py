454e4fbb62b3b8cf69b0dfbaf9887a5882aee75b
Josh LaFayette <joshlafayette@gmail.com>
2022-04-20 18:15:08 -0700

    Refactor 2021 day1
    - Extracted parsing to it's own function
    - Renamed numbers -> depths
    - Added test cases for the full puzzle input

--- 2021/day01/main.py
+++ 2021/day01/main.py
@@ -1,5 +1,5 @@
-from cmath import exp
 from pathlib import Path
+from typing import List
 
 
 TEST_INPUT = """199
@@ -15,12 +15,22 @@ TEST_INPUT = """199
 """
 
 
-def part1(data: str) -> int:
-    numbers = [int(x) for x in data.split("\n") if x]
+def parse(data: str) -> List[int]:
+    return [int(x) for x in data.split("\n") if x]
+
+
+def read_input() -> List[int]:
+    input_file = Path(__file__).parent / "input"
+    contents = input_file.read_text()
+    depths = parse(contents)
+    return depths
+
+
+def part1(depths: List[int]) -> int:
     increase_count = 0
-    for i, n1 in enumerate(numbers):
+    for i, n1 in enumerate(depths):
         try:
-            n2 = numbers[i+1]
+            n2 = depths[i+1]
         except IndexError:
             break
         if n2 > n1:
@@ -28,24 +38,33 @@ def part1(data: str) -> int:
     return increase_count
 
 
-def test_part1():
-    assert part1(TEST_INPUT) == 7
+def test_part1() -> None:
+    depths = parse(TEST_INPUT)
+    assert part1(depths) == 7
 
 
-def part2(data: str) -> int:
-    numbers = [int(x) for x in data.split("\n") if x]
+def test_part1_full_input() -> None:
+    depths = read_input()
+
+    result = part1(depths)
+
+    expected = 1791
+    assert result == expected
+
+
+def part2(depths: List[int]) -> int:
     increase_count = 0
-    for i, n1 in enumerate(numbers):
+    for i, n1 in enumerate(depths):
         try:
-            n2 = numbers[i+1]
+            n2 = depths[i+1]
         except IndexError:
             break
         try:
-            n3 = numbers[i+2]
+            n3 = depths[i+2]
         except IndexError:
             break
         try:
-            n4 = numbers[i+3]
+            n4 = depths[i+3]
         except IndexError:
             break
 
@@ -57,21 +76,31 @@ def part2(data: str) -> int:
     return increase_count
 
 
-def test_part2():
-    result = part2(TEST_INPUT)
+def test_part2() -> None:
+    depths = parse(TEST_INPUT)
+
+    result = part2(depths)
+
     expected = 5
     assert result == expected
 
 
-def main():
-    # read puzzle input from file
-    input_file = Path(__file__).parent / "input"
-    contents = input_file.read_text()
+def test_part2_full_input() -> None:
+    depths = read_input()
 
-    part1_answer = part1(contents)
+    result = part2(depths)
+
+    expected = 1822
+    assert result == expected
+
+
+def main() -> None:
+    depths = read_input()
+
+    part1_answer = part1(depths)
     print(part1_answer)
 
-    part2_answer = part2(contents)
+    part2_answer = part2(depths)
     print(part2_answer)
 
 
