7d6381749b1c550edbea3adf56f3184dcf54a8ad
Josh LaFayette <joshlafayette@gmail.com>
2022-04-23 14:45:30 -0700

    WIP 2021 day3 part2
    part1 time 39:06
    part2 time 31:17
    
    had to end stream, still need to wrap up part 2

--- 2021/day03/main.py
+++ 2021/day03/main.py
@@ -1,3 +1,4 @@
+from copy import copy
 from collections import Counter
 from pathlib import Path
 
@@ -15,7 +16,6 @@ def part1(data: str) -> int:
         least_common += counter.most_common()[-1][0]
 
     return int(most_common, base=2) * int(least_common, base=2)
-
 
 
 TEST_INPUT = """00100
@@ -48,11 +48,57 @@ def test_part1_actual_data():
     assert actual == expected
 
 
+def part2(report: str) -> int:
+    lines = report.strip().split("\n")
+    length = len(lines[0])
+
+    most_common = ""
+    least_common = ""
+
+    for position in range(length):
+        counter = Counter([x[position] for x in lines])
+        most_common += counter.most_common()[0][0]
+        least_common += counter.most_common()[-1][0]
+
+    # oxygen generator rating
+    # filter to most common (ties keep values with 1)
+    ox_gen_str = ""
+    ox_lines = copy(lines)
+    for position in range(length):
+        counter = Counter([x[position] for x in ox_lines])
+        ms_pos = counter.most_common()[0][0]
+        # [('0', 1), ('1', 1)]
+        mc = counter.most_common()
+        equal = mc[0][1] == mc[1][1]
+
+        def f(line: str) -> bool:
+            if equal:
+                return line[position] == '1'
+            else:
+                return line[position] == ms_pos
+
+        ox_lines = [
+            line for line in ox_lines
+            if f(line)
+        ]
+        if len(ox_lines) == 1:
+            ox_gen_str = ox_lines[0]
+
+    # TODO: calculate CO2 scrubber rating
+    # ...
+
+    # return int(most_common, base=2) * int(least_common, base=2)
+    return 0
+
+
 def main() -> None:
     report = Path("input.txt").read_text()
 
     answer1 = part1(report)
     print(answer1)  # 2972336
+
+    answer2 = part2(TEST_INPUT)
+    print(answer2)  #
 
 
 if __name__ == "__main__":
