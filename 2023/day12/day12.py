3b9a9c3aee8d8475e3df82bc4ee1918bcd4f86f6
Josh LaFayette <joshlafayette@gmail.com>
2023-12-27 17:20:56 -0800

    2023 day12 python (part 2 wip)

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -193,73 +193,98 @@ class Part2:
 
     def solve(self, line):
         statuses, grps = self.parse(line)
-        return self.r_solve(statuses, grps, 0)
+        return self.r_solve(statuses, grps)
 
     @staticmethod
-    def _take_next_grp(statuses: list[str]) -> tuple[int, list[str]]:
-        qs = 0
-        for i, ch in enumerate(statuses):
+    def fully_resolved(statuses: list[str]) -> bool:
+        for ch in statuses:
             if ch == "?":
-                qs += 1
-            else:
-                if qs:
-                    return qs, statuses[i+1:]
-        else:
-            return 0, statuses
+                return False
+        return True
 
-    def r_solve(self, statuses: list[str], grps: list[int], damaged: int):
-        # take first ? group
-        ss = ""
+    @staticmethod
+    def is_valid(statuses: list[str], grps: list[int]) -> bool:
+        group_stack = list(reversed(record.grps))
+        group = group_stack.pop()
+        last = "."
+        current_br = 0
+        for s in record.statuses:
+            if s == "#":
+                current_br += 1
+                # if current_br > group:
+                #     # print(f"{current_br}>{group}")
+                #     return False
+            if last == "#" and s != "#":
+                if current_br != group:
+                    # print(f"{current_br}!={group}")
+                    return False
+                try:
+                    group = group_stack.pop()
+                except IndexError:
+                    group = 0
+                current_br = 0
+            if s == "?":
+                raise ValueError("No UNKNOWN statuses allowed")
+            last = s
+        return current_br == group and len(group_stack) == 0
+
+    def r_solve(self, statuses: list[str], grps: list[int]):
+
         while True:
-            ss, *statuses = statuses
-            if ss == "?":
-                break
-            if not statuses:
-                # done
-                if not grps:
+            if self.fully_resolved(statuses):
+                if self.is_valid(statuses, grps):
                     return 1
                 else:
                     return 0
 
-        if not grps:
-            return 0
+            a_statuses = []  # ["."] + statuses
+            b_statuses = []  # ["#"] + statuses
 
-        # solve rest with both '.' and '#'
-        a_statuses = ["."] + statuses
-        b_statuses = ["#"] + statuses
-        a_damaged = 0
-        b_damaged = damaged + 1
-        a_arrs = self.r_solve(statuses, grps, a_damaged)
-
-        # split group possibilities
-        if b_damaged >= grps[0]:
-            ...
-
-        # add solutions together
-        ...
-
-    def r_solve_multi(self, statuses: list[str], possible_grps: list[list[int]]):
-        total = 0
-        for grps in possible_grps:
-            total += self.r_solve(statuses, grps)
-        return total
+            for i, ch in enumerate(statuses):
+                if ch != "?":
+                    a_statuses.append(ch)
+                    b_statuses.append(ch)
+                    continue
+                a_statuses.append(".")
+                b_statuses.append("#")
+                a_statuses.extend(statuses[i:])
+                b_statuses.extend(statuses[i:])
+                break
+            
+            # solve rest with both '.' and '#'
+            a_arrs = self.r_solve(a_statuses, grps)
+            b_arrs = self.r_solve(b_statuses, grps)
+            return a_arrs + b_arrs
 
     def __call__(self, *args, **kwargs):
         print("part2")
         line = ".# 1"
-        statuses, groups = parse(line)
+        statuses, groups = self.parse(line)
         assert statuses == [ch for ch in ".#?.#?.#?.#?.#"]
         assert groups == [1, 1, 1, 1, 1]
 
-        statuses = "?###????????"
+        # def split_and_strip(data: str, sep: str) -> list[str]:
+        for line in split_and_strip(self.data, "\n"):
+            statuses, groups = self.parse(line)
+            r = self.solve(line)
+            print(line)
+            print("  ", r)
 
 
 def part2(data):
+    data = """\
+???.### 1,1,3
+.??..??...?##. 1,1,3\
+"""
+    # ?#?#?#?#?#?#?#? 1,3,1,6
+    # ????.#...#... 4,1,1
+    # ????.######..#####. 1,6,5
+    # ?###???????? 3,2,1
     Part2(data)()
 
 
 # part1(NO_UNKNOWNS)
 # part1(DATA)
 # part1(INPUT)
-# part2(DATA)
+part2(DATA)
 # part2(INPUT)
